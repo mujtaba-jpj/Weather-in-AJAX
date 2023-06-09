b68b23405cb1121e1aebb1b3920a1a192f3af07e
Mujtaba Abbas <mujtababbas13@gmail.com>
2023-06-09 19:06:08 +0500

    updated views.py

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -12,7 +12,6 @@ import datetime
 def test(keys, values):
     return dict(zip(keys, values))
 
-@csrf_exempt
 def weather(request):
     if request.method == 'POST':
         search_query = request.POST['search_query']
